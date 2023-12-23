80b17ee1db124186a23e6e133c6685580a1a57fb
Bernhard <bernhard.mail.it@gmail.com>
2023-12-24 00:19:07 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/* skript not in use */
+
+var merrychristmas = document.getElementById("audio1");
+var jinglemiau = documnt.getElementById("audio2")
+var lastmiau = document.getElementById("audio3")
+
+function playMerryChristmas() {
+    merrychristmas.play();
+}
+
+function playJinglebells(){
+    jinglemiau.play();
+}
+
+function playLastmiaumiau(){
+    lastmiau.play();
+}
+
+function playAll(){
+    merrychristmas.play();
+    jinglemiau.play();
+    lastmiau.play();
+}
